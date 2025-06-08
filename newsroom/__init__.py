6f7788efab5766a2c0eeff8e14c33f25016fdd85
Daryl Kang <darylkang@apple.com>
2025-06-07 20:44:23 -0400

    Create __init__.py

---
+++ newsroom/__init__.py
@@ -0,0 +1,8 @@
+"""
+newsroom - A news digest generator for Astro sites.
+
+This package scrapes various news sources and generates markdown files
+for consumption by Astro-based static sites.
+"""
+
+__version__ = "0.1.0" 
